3beec15589dd565541a8d6afd484e37627eb8d01
Leo Ramos <leo.o.rms@gmail.com>
2017-10-25 11:37:07 -0200

    tabelas de tipo de ocorrencia criadas

--- scripts/organize_csvs2.py
+++ scripts/organize_csvs2.py
@@ -80,6 +80,13 @@ list_of_files.append(path_out + 'endereco.csv')
 list_of_files.append(path_out + 'veiculo.csv')
 list_of_files.append(path_out + 'objeto.csv')
 
+list_of_files.append(path_out + 'les_corporal.csv')
+list_of_files.append(path_out + 'morte_susp.csv')
+list_of_files.append(path_out + 'homicidio.csv')
+list_of_files.append(path_out + 'roubo.csv')
+list_of_files.append(path_out + 'furto.csv')
+list_of_files.append(path_out + 'outras_oco.csv')
+
 # Printing list of files
 # for st in list_of_files:
 #     print(st)
@@ -107,14 +114,22 @@ id_pessoa = 1
 id_ocorrencia = 1
 id_bo = 1
 id_endereco = 1
-id_veiculo = 1
+id_objeto = 1
 
 delegacias = dict()
 pessoas = dict()
 ocorrencias = dict()
 bos = dict()
 enderecos = dict()
-veiculos = dict()
+objetos = dict()
+
+veiculos = set()
+les_corporal_tab = set()
+morte_suspeita_tab = set()
+homicidio_tab = set()
+outras_oc_tab = set()
+roubo_tab = set()
+furto_tab = set()
 
 # Reading data
 for row in ur:
@@ -127,12 +142,49 @@ for row in ur:
         delegacias[delegacia] = id_delegacia
         id_delegacia += 1
 
+    # pessoa = NOMEPESSOA, TIPOPESSOA, VITIMAFATAL, RG, RG_UF, NATURALIDADE, NACIONALIDADE, SEXO, 
+    #   DATANASCIMENTO, IDADE, ESTADOCIVIL, PROFISSAO, GRAUINSTRUCAO, CORCUTIS, NATUREZAVINCULADA, TIPOVINCULO
+    pessoa = (row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36], row[37], row[38], row[39], row[40], row[41], row[42], row[43])
+    if pessoa not in pessoas:
+        pessoas[pessoa] = id_pessoa
+        id_pessoa += 1
+
     # Veiculo = placa, UF, CIDADE, COR, MARCA, ANO_FABRICACAO, ANO_MODELO, TIPO_VEICULO
     veiculo = (row[44], row[45], row[46], row[47], row[48], row[49], row[50], row[51])
 
     if veiculo not in veiculos:
-        veiculos[veiculo] = id_veiculo
-        id_veiculo += 1
+        veiculos.add(veiculo)
+
+    # objeto = nome
+    if (row[25].lower().find(u'roubo') != -1) or (row[25].lower().find(u'furto') != -1):
+        if row[25].lower().find(u'interior de veiculo') != -1:
+            objeto = (u'interior de veiculo', u'0')
+
+        elif row[25].lower().find(u'veículo') != -1 or row[25].lower().find(u'veiculo') != -1:
+            objeto = (u'veiculo', str(veiculo[0]))
+
+        elif row[25].lower().find(u'transeunte') != -1:
+            objeto = (u'transeunte', u'0')
+
+        elif row[25].lower().find(u'estabelecimento') != -1:
+            objeto = (u'estabelecimento comercial', u'0')
+
+        elif row[25].lower().find(u'carga') != -1:
+            objeto = (u'outros', u'0')
+
+        elif row[25].lower().find(u'residencia') != -1:
+            objeto = (u'residencia', u'0')
+
+        elif row[25].lower().find(u'interior de transporte') != -1:
+            objeto = ( u'interior transporte coletivo', u'0')
+
+        else:
+            objeto = (u'outros', u'0')
+
+        if objeto not in objetos:
+            objetos[objeto] = id_objeto
+            id_objeto += 1
+
 
     # Endereco = LOGRADOURO, NUMERO, BAIRRO, CIDADE, UF, LATITUDE, LONGITUDE, DESCRICAOLOCAL
     endereco = (row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19])
@@ -150,16 +202,40 @@ for row in ur:
     # ocorrencia = ID_BO, DATAOCORRENCIA, PERIDOOCORRENCIA, DATACOMUNICACAO, FLAGRANTE, ID_ENDERECO, EXAME, ESPECIE, RUBRICA, DESDOBRAMENTO, STATUS
     ocorrencia = (str(bos[bo]), row[5], row[6], row[7], row[10], str(enderecos[endereco]), row[20], row[24], row[25], row[26], row[27])
     if ocorrencia not in ocorrencias:
+        # Tratando os tipos de ocorrencia
+        if row[25].lower().find(u'lesão') != -1:
+
+            if row[25].lower().find(u'morte') != -1:
+                les_corporal = (str(id_ocorrencia), u'S', str(pessoas[pessoa]))
+            else:
+                les_corporal = (str(id_ocorrencia), u'N', str(pessoas[pessoa]))
+            les_corporal_tab.add(les_corporal)
+
+        elif row[25].lower().find(u'morte') != -1:
+            morte_susp = (str(id_ocorrencia), u'', str(pessoas[pessoa]))
+            morte_suspeita_tab.add(morte_susp)
+
+        elif row[25].lower().find(u'homicídio') != -1:
+            if row[25].lower().find(u'simples') != -1:
+                homicidio = (str(id_ocorrencia), u'simples', str(pessoas[pessoa]))
+            else:
+                homicidio = (str(id_ocorrencia), u'qualificado', str(pessoas[pessoa]))
+            homicidio_tab.add(homicidio)
+
+        elif row[25].lower().find(u'roubo') != -1:
+            roubo = (str(id_ocorrencia), str(pessoas[pessoa]), str(objetos[objeto]))
+            roubo_tab.add(roubo)
+
+        elif row[25].lower().find(u'furto') != -1:
+            furto = (str(id_ocorrencia), str(pessoas[pessoa]), str(objetos[objeto]))
+            furto_tab.add(furto)
+        else:
+            outro = (str(id_ocorrencia), str(pessoas[pessoa]))
 
         ocorrencias[ocorrencia] = id_ocorrencia
         id_ocorrencia += 1
 
-    # pessoa = NOMEPESSOA, TIPOPESSOA, VITIMAFATAL, RG, RG_UF, NATURALIDADE, NACIONALIDADE, SEXO, 
-    #   DATANASCIMENTO, IDADE, ESTADOCIVIL, PROFISSAO, GRAUINSTRUCAO, CORCUTIS, NATUREZAVINCULADA, TIPOVINCULO
-    pessoa = (row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36], row[37], row[38], row[39], row[40], row[41], row[42], row[43])
-    if pessoa not in pessoas:
-        pessoas[pessoa] = id_pessoa
-        id_pessoa += 1
+    
 
 
 # Delegacia
@@ -190,7 +266,7 @@ for o in ocorrencias:
     o.append(str(id_ocorrencia))
     ocorrencias_l.append(o)
 
-uw_list[2].writerows(l_ocorrencias)
+uw_list[2].writerows(ocorrencias_l)
 
 # BOs
 bos_l = []
@@ -200,21 +276,48 @@ for b in bos:
     b.append(str(id_bo))
     bos_l.append(b)
 
-uw_list[3].writerows(bos)
+uw_list[3].writerows(bos_l)
 
 # Enderecos
 enderecos_l = []
 for e in enderecos:
-    id_endereco = enderecos[endereco]
+    id_endereco = enderecos[e]
     e = list(e)
     e.append(str(id_endereco))
     enderecos_l.append(e)
 
-uw_list[4].writerows(enderecos)
+uw_list[4].writerows(enderecos_l)
 
 # Veiculos
 uw_list[5].writerows(veiculos)
 
+# Objetos
+objetos_l = []
+for o in objetos:
+    id_objeto = objetos[o]
+    o = list(o)
+    o.append(str(id_objeto))
+    objetos_l.append(o)
+
+uw_list[6].writerows(objetos_l)
+
+# Lesao corporal
+uw_list[7].writerows(les_corporal_tab)
+
+# Morte suspeita
+uw_list[8].writerows(morte_suspeita_tab)
+
+# Homicidio
+uw_list[9].writerows(homicidio_tab)
+
+# Roubo
+uw_list[10].writerows(roubo_tab)
+
+# Furto
+uw_list[11].writerows(furto_tab)
+
+# Outras Ocorrencias
+uw_list[12].writerows(outras_oc_tab)
 
 # Closing files
 arq.close()
